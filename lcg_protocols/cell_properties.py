613f2fec9bae76ef99caf63bb13009e4e201dbc0
Joao Couto <jpcouto@gmail.com>
2014-11-16 18:17:18 +0100

    add protocols

---
+++ lcg_protocols/cell_properties.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import subprocess as sub
+
+# Run the a protocol
+
+# Find AP reobase (ramp)
+
+# Find the current that hyperpolarizes the cell by 5mV
+
+# Run step for the hyper and depol 
+
+# Example of the cell properties (plotting and values?)
